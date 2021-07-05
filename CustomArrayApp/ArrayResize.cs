689edbb510d596e0581db98cf264474e98c8788e
aimanshariff31 <86050998+aimanshariff31@users.noreply.github.com>
2021-07-05 16:59:03 +0530

    Add files via upload

---
+++ CustomArrayApp/ArrayResize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomArrayApp
+{
+    public class ArrayResize
+    {
+        public static void Resize(ref int[] array, int newSize)
+        {
+            int[] numbers = new int[newSize];
+            for (int i = 0; i < array.Length; i++)
+            {
+                numbers[i] = array[i];
+            }
+            array = numbers;
+
+        }
+    }
+}
